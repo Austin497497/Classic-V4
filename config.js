07de3aa4d20cd3b6136f354731f7c83613e8f1e0
Austin497497 <austinodundo497@gmail.com>
2025-01-16 21:31:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ require("./doc/module.js")
 */
 
 global.prefix = ['','!','.',',','馃悿','馃椏']
-global.owner = ['254742491666']
-global.ownMain = '254742491666'
-global.NamaOwner = '饾暥饾枎饾枔饾枌 饾暰饾枂饾枓' //
+global.owner = ['254757725656']
+global.ownMain = '254757725656'
+global.NamaOwner = '★DRACO★' //
 global.sessionName = 'session'
 global.connect = true // 
 global.namabot = 'CLASSIC-V4' //
